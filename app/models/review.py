bbe12e7a1a1d73d4182cf10eb2c5f537b618b149
Raymond Nehring <rnehring89@gmail.com>
2021-12-14 16:32:29 -0600

    review model

---
+++ app/models/review.py
@@ -0,0 +1,26 @@
+from .db import db
+from sqlalchemy import DateTime
+import datetime
+
+class Review(db.Model):
+    __tablename__ = 'reviews'
+
+    id = db.Column(db.Integer, primary_key=True)
+    review = db.Column(db.String(500), nullable=False)
+    created_at = db.Column(DateTime, default=datetime.datetime.utcnow)
+    updated_at = db.Column(DateTime, default=datetime.datetime.utcnow)
+
+    userId = db.Column(db.Integer, db.ForeignKey('users.id'))
+    user = db.relationship("User", back_populates="spots")
+    
+    spotId = db.Column(db.Integer, db.ForeignKey('spots.id'))
+    spot = db.relationship("Spot", back_populates="reviews")
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "spotId": self.spotId,
+            "review": self.review,
+            "userId": self.userId
+            # "comments": [comment.to_dict() for comment in self.comments],
+        }
