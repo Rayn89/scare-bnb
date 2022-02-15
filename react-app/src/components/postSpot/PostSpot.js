903031f93b384b590caf5516aebd462e377979b2
Raymond Nehring <rnehring89@gmail.com>
2022-02-15 17:04:23 -0600

    starting post spot MUI form

--- react-app/src/components/postSpot/PostSpot.js
+++ react-app/src/components/postSpot/PostSpot.js
@@ -10,6 +10,12 @@ import "./PostSpot.css"
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import InputAdornment from '@mui/material/InputAdornment';
+import OutlinedInput from '@mui/material/OutlinedInput';
 
 const CreateSpotForm = () => {
   const [errors, setErrors] = useState([]);
@@ -114,163 +120,266 @@ const CreateSpotForm = () => {
   }
 
   return (
-    <section className="section-container">
-      <div className="form-container">
-        <form className="main-form-container" onSubmit={handleSubmit}>
-          <h3 className="new-spot-header">Add a Spot</h3>
-          <div className="error-list-container">
-            <ul className="error-list">
-            {errors.map((error, ind) => (
-              <li className="errors" key={ind}>{error}</li>
-            ))}
-            </ul>
-          </div>
-          <div className="input-field-new">
-            {/* <input
-              className="new-spot-input"
-              name="name"
-              type="input"
-              placeholder="Spot Name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            ></input> */}
-            
-          </div>
-           <Box
-            component="form"
-            sx={{
-              '& > :not(style)': { m: 1, width: '40ch' },
-            }}
-            noValidate
-            autoComplete="off"
-            onChange={(e) => setName(e.target.value)}
-          >
-          <TextField id="outlined-basic" label="Spot Name" variant="outlined" />
-          </Box>
-          <div className="input-field-new">
-            <input
-              className="new-spot-input"
-              name="address"
-              type="input"
-              placeholder="Address"
-              value={address}
-              onChange={(e) => setAddress(e.target.value)}
-            ></input>
-          </div>
-          <div className="input-field-new">
-            <input
-              className="new-spot-input"
-              name="city"
-              type="input"
-              placeholder="City"
-              value={city}
-              onChange={(e) => setCity(e.target.value)}
-            ></input>
-          </div>
-          <div className="input-field-new">
-            <input
-              className="new-spot-input"
-              name="state"
-              type="input"
-              placeholder="State"
-              value={state}
-              onChange={(e) => setState(e.target.value)}
-            ></input>
-          </div>
+    <div className="login-form">
+      <h2 className="login-header">Please Signup</h2>
+      <div className="login-email">
+        <Box
+          component="form"
+          // onSubmit={onSignUp}
+          sx={{
+            '& > :not(style)': { m: 1, width: '100%' },
+          }}
+          // noValidate
+          // className="login-email"
+          autoComplete="off"
+        >
+        {errors ? <TextField value={name}
+          onChange={(e) => setName(e.target.value)} fullWidth id="outlined-basic" label="Spot Name" variant="outlined" /> : 
+        
+        <TextField
+          error
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          id="outlined-error-helper-text"
+          label="Spot Name"
+          // helperText={usernameError}
+        />
+        }
+        {errors ? <TextField value={address}
+          onChange={(e) => setAddress(e.target.value)} fullWidth id="outlined-basic" label="Address" variant="outlined" /> : 
+        
+        <TextField
+          error
+          value={address}
+          onChange={(e) => setAddress(e.target.value)}
+          id="outlined-error-helper-text"
+          label="Address"
+          // helperText={emailError}
+        />
+        }
+        {errors ? <TextField type="password" value={city}
+          onChange={(e) => setCity(e.target.value)} fullWidth id="outlined-basic" label="City" variant="outlined" /> : 
+        
+        <TextField
+          error
+          name="password"
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+          id="outlined-error-helper-text"
+          label="City"
+          // helperText={passwordError}
+        />
+        }
+        {errors ? <TextField type="password" value={state}
+          onChange={(e) => setState(e.target.value)} fullWidth id="outlined-basic" label="State" variant="outlined" /> : 
+        
+        <TextField
+          error
+          name="password"
+          value={state}
+          onChange={(e) => setState(e.target.value)}
+          id="outlined-error-helper-text"
+          label="State"
+          // helperText={repeatError}
+        />
+        }
+        {errors ? <TextField value={country}
+          onChange={(e) => setCountry(e.target.value)} fullWidth id="outlined-basic" label="Country" variant="outlined" /> : 
+        
+        <TextField
+          error
+          name="password"
+          value={country}
+          onChange={(e) => setCountry(e.target.value)}
+          id="outlined-error-helper-text"
+          label="Country"
+          // helperText={repeatError}
+        />
+        }
+        <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">Age</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          // value={age}
+          label="Age"
+          // onChange={handleChange}
+        >
+          <MenuItem value={10}>Ten</MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem>
+        </Select>
+        </FormControl>
 
-          <div className="input-field-new">
-            <input
-              className="new-spot-input"
-              name="country"
-              type="input"
-              placeholder="Country"
-              value={country}
-              onChange={(e) => setCountry(e.target.value)}
-            ></input>
-          </div>
-          <div>
-            <label>
-              Please select price per night:
-              <input
-                className="price-per-night"
-                name="price"
-                type="number"
-                placeholder="Price"
-                min="0"
-                max="1000"
-                step="50"
-                value={price}
-                onChange={(e) => setPrice(e.target.value)}
-              ></input>
-            </label>
-          </div>
-          <div>
-            <label>
-              Select type of Haunting:
-              <select
-                className="price-per-night"
-                value={haunting}
-                onChange={(e) => setHaunting(e.target.value)}
-              >
-                <option value=""> </option>
-                <option value="Ghoul">Ghoul</option>
-                <option value="Demon">Demon</option>
-                <option value="Spirit">Spirit</option>
-                <option value="Bladefingers">BladeFingers</option>
-              </select>
-            </label>
-          </div>
-          <h2 className="new-post-photo-header">Please add three images:</h2>
-          <div className="new-post-photo-container">
-            <div className="content1-container">
-              {content}
-              <input
-                type="url"
-                placeholder="Main image URL"
-                className="new-spot-input"
-                onChange={(e) => {
-                  setImage1(e.target.value);
-                }}
-                required
-              />
-            </div>
-            <div className="content1-container">
-              {content2}
-              <input
-                type="url"
-                className="new-spot-input"
-                placeholder="Additional Image"
-                onChange={(e) => {
-                  setImage2(e.target.value);
-                }}
-                required
-              />
-            </div>
-            <div className="content1-container">
-              {content3}
-              <input
-                type="url"
-                className="new-spot-input"
-                placeholder="Additional Image"
-                onChange={(e) => {
-                  setImage3(e.target.value);
-                }}
-                required
-              />
-            </div>
-          </div>
-          {/* <button className="post-spot-form-button" type="submit">
-            Submit Spot
-          </button> */}
-          <Button style={{
-        borderRadius: 10,
-        backgroundColor: "#FF385C",
-        marginTop: 20,
-        marginBottom: 20,
-        }} type="submit" variant="contained">Submit Spot</Button>
-        </form>
+        <FormControl fullWidth sx={{ m: 1 }}>
+          <InputLabel htmlFor="outlined-adornment-amount">Amount</InputLabel>
+          <OutlinedInput
+            id="outlined-adornment-amount"
+            // value={values.amount}
+            inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }}
+            // onChange={handleChange('amount')}
+            startAdornment={<InputAdornment position="start">$</InputAdornment>}
+            label="Amount"
+          />
+        </FormControl>
+
+        <div className="login-demo-buttons">
+        <Button style={{
+          borderRadius: 10,
+          backgroundColor: "#FF385C",
+          marginBottom: 20,
+          }} 
+          onClick={() => {
+                {console.log(errors)}
+              }} 
+          className="MUI-login-button" 
+          type="submit" variant="contained">Signup
+        </Button>
+        
+        </div>
+        </Box>
       </div>
-    </section>
+    </div>
+    // <section className="section-container">
+    //   <div className="form-container">
+    //     <form className="main-form-container" onSubmit={handleSubmit}>
+    //       <h3 className="new-spot-header">Add a Spot</h3>
+    //       <div className="error-list-container">
+    //         <ul className="error-list">
+    //         {errors.map((error, ind) => (
+    //           <li className="errors" key={ind}>{error}</li>
+    //         ))}
+    //         </ul>
+    //       </div>
+    //       <div className="input-field-new">
+    //         <input
+    //           className="new-spot-input"
+    //           name="name"
+    //           type="input"
+    //           placeholder="Spot Name"
+    //           value={name}
+    //           onChange={(e) => setName(e.target.value)}
+    //         ></input>
+            
+    //       </div>
+    //       <div className="input-field-new">
+    //         <input
+    //           className="new-spot-input"
+    //           name="address"
+    //           type="input"
+    //           placeholder="Address"
+    //           value={address}
+    //           onChange={(e) => setAddress(e.target.value)}
+    //         ></input>
+    //       </div>
+    //       <div className="input-field-new">
+    //         <input
+    //           className="new-spot-input"
+    //           name="city"
+    //           type="input"
+    //           placeholder="City"
+    //           value={city}
+    //           onChange={(e) => setCity(e.target.value)}
+    //         ></input>
+    //       </div>
+    //       <div className="input-field-new">
+    //         <input
+    //           className="new-spot-input"
+    //           name="state"
+    //           type="input"
+    //           placeholder="State"
+    //           value={state}
+    //           onChange={(e) => setState(e.target.value)}
+    //         ></input>
+    //       </div>
+
+    //       <div className="input-field-new">
+    //         <input
+    //           className="new-spot-input"
+    //           name="country"
+    //           type="input"
+    //           placeholder="Country"
+    //           value={country}
+    //           onChange={(e) => setCountry(e.target.value)}
+    //         ></input>
+    //       </div>
+    //       <div>
+    //         <label>
+    //           Please select price per night:
+    //           <input
+    //             className="price-per-night"
+    //             name="price"
+    //             type="number"
+    //             placeholder="Price"
+    //             min="0"
+    //             max="1000"
+    //             step="50"
+    //             value={price}
+    //             onChange={(e) => setPrice(e.target.value)}
+    //           ></input>
+    //         </label>
+    //       </div>
+    //       <div>
+    //         <label>
+    //           Select type of Haunting:
+    //           <select
+    //             className="price-per-night"
+    //             value={haunting}
+    //             onChange={(e) => setHaunting(e.target.value)}
+    //           >
+    //             <option value=""> </option>
+    //             <option value="Ghoul">Ghoul</option>
+    //             <option value="Demon">Demon</option>
+    //             <option value="Spirit">Spirit</option>
+    //             <option value="Bladefingers">BladeFingers</option>
+    //           </select>
+    //         </label>
+    //       </div>
+    //       <h2 className="new-post-photo-header">Please add three images:</h2>
+    //       <div className="new-post-photo-container">
+    //         <div className="content1-container">
+    //           {content}
+    //           <input
+    //             type="url"
+    //             placeholder="Main image URL"
+    //             className="new-spot-input"
+    //             onChange={(e) => {
+    //               setImage1(e.target.value);
+    //             }}
+    //             required
+    //           />
+    //         </div>
+    //         <div className="content1-container">
+    //           {content2}
+    //           <input
+    //             type="url"
+    //             className="new-spot-input"
+    //             placeholder="Additional Image"
+    //             onChange={(e) => {
+    //               setImage2(e.target.value);
+    //             }}
+    //             required
+    //           />
+    //         </div>
+    //         <div className="content1-container">
+    //           {content3}
+    //           <input
+    //             type="url"
+    //             className="new-spot-input"
+    //             placeholder="Additional Image"
+    //             onChange={(e) => {
+    //               setImage3(e.target.value);
+    //             }}
+    //             required
+    //           />
+    //         </div>
+    //       </div>
+    //       <button className="post-spot-form-button" type="submit">
+    //         Submit Spot
+    //       </button>
+    //     </form>
+    //   </div>
+    // </section>
   );
 };
 
